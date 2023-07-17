019f369e0994b0b5b69ea3d1f6ebc04dc993fab9
Evgeny Kravets <kravets@netcube.ru>
2023-07-17 15:33:16 +0300

    init

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import "./Popup.css";
+const Popup = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+export default Popup;
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    popup: path.join(__dirname, 'src/index.js'),
+    service_worker: path.join(__dirname, 'src/background'),
+    content: path.join(__dirname, 'src/content'),
+  },
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'js/[name].js',
+    clean: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-flexbugs-fixes',
+                  [
+                    'postcss-preset-env',
+                    {
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/popup.html',
+      filename: 'popup.html',
+      excludeChunks: ['background'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'),
+          to: path.resolve(__dirname, 'build'),
+          globOptions: {
+            ignore: ['**/popup.html'],
+          },
+        },
+      ],
+    }),
+  ],
+};
